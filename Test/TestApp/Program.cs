a2899529a44e09d9fb71cbda21cf0b53da0a3441
Neil Mackey <neilmackey@gmail.com>
2020-10-03 16:25:32 +0100

    Cleanup TestApp a bit

--- Test/TestApp/Program.cs
+++ Test/TestApp/Program.cs
@@ -12,23 +12,24 @@ namespace TestApp
     {
         static async Task Main(string[] args)
         {
-            var serviceCollection = new ServiceCollection();
             var config = new ConfigurationBuilder()
                 .AddUserSecrets<Program>()
                 .Build();
             
-            serviceCollection.AddSingleton<IConfiguration>(config);
-            serviceCollection.AddDbContext<GamesContext>(options => options.UseSqlServer("Name=Games"));
-            var services = serviceCollection.BuildServiceProvider();
+            var services = new ServiceCollection()
+                .AddSingleton<IConfiguration>(config)
+                .AddDbContext<GamesContext>(options => options.UseSqlServer("Name=Games"))
+                .BuildServiceProvider();
+            
             var gamesContext = services.GetService<GamesContext>();
 
-            await gamesContext.Games.AddRangeAsync(new[]
-            {
-                new Game { Name = "Super Mario 64" },
-                new Game { Name = "Super Mario Sunshine" },
-                new Game { Name = "Super Mario Galaxy" }
-            });
-            await gamesContext.SaveChangesAsync();
+            // await gamesContext.Games.AddRangeAsync(new[]
+            // {
+            //     new Game { Name = "Super Mario 64" },
+            //     new Game { Name = "Super Mario Sunshine" },
+            //     new Game { Name = "Super Mario Galaxy" }
+            // });
+            // await gamesContext.SaveChangesAsync();
             
             var games = gamesContext.Games.ToList();
             Console.ReadKey();
