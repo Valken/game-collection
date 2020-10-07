ae4517f3fb97800b2e1b3a9b1e81a69d654cb01b
Neil Mackey <neilmackey@gmail.com>
2020-10-07 20:46:04 +0100

    Add SystemController with POST and DELETE support, need to figure out model binding and what route will like like for PUTs

--- Source/GameCollection.WebApi/Controllers/SystemController.cs
+++ Source/GameCollection.WebApi/Controllers/SystemController.cs
@@ -1,7 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameCollection.Database;
+using GameCollection.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
 namespace GameCollection.WebApi.Controllers
 {
-    public class SystemController
+    [ApiController]
+    [Route("[controller]")]
+    public class SystemController : ControllerBase
     {
-        
+        private readonly GamesContext _gamesContext;
+        private readonly ILogger<SystemController> _logger;
+
+        public SystemController(GamesContext gamesContext, ILogger<SystemController> logger)
+        {
+            _gamesContext = gamesContext ?? throw new ArgumentNullException(nameof(gamesContext));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        public IEnumerable<GameCollection.Database.Models.System> List() => _gamesContext.Systems;
+
+        [HttpGet("{id}")]
+        public GameCollection.Database.Models.System Get(int id) =>
+            _gamesContext.Systems.SingleOrDefault(g => g.SystemId == id);
+
+        [HttpPost]
+        public void Create([FromBody] GameCollection.Database.Models.System system)
+        {
+            _gamesContext.Add(system);
+            _gamesContext.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var system = _gamesContext.Systems.Find(id);
+            _gamesContext.Remove(system);
+            _gamesContext.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody]GameCollection.Database.Models.System system)
+        {
+            throw new ArgumentNullException(nameof(Update));
+        }
     }
 }
